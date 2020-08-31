9489f886efe375df56abe6d884a022fe22755386
klvo <klvo.gtfo.gmail.com>
2020-08-31 14:40:32 -0300

    Main View design

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,8 +13,12 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load(); // Obj principal da minha view que no caso é o anchorPane vazio
-			Scene mainScene = new Scene(parent);
+			ScrollPane scrollPane = loader.load(); // Obj principal da minha view que no caso é o anchorPane vazio
+			
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
+			Scene mainScene = new Scene(scrollPane); 
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
