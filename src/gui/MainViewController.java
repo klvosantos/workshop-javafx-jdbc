901636f9860bb28bb9d1d064cd4cffcd02a7771f
klvo <klvo.gtfo.gmail.com>
2020-09-01 15:02:13 -0300

    Initializing action as parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -33,12 +34,15 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
 	}
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 
 	@Override
@@ -46,7 +50,7 @@ public class MainViewController implements Initializable{
 		
 	}
 	
-	private synchronized void loadView(String absoluteName) { // synchronized garante que processamento entre o try não vai ser interrompido durante o multthread
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) { // synchronized garante que processamento entre o try não vai ser interrompido durante o multthread
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			VBox newVBox = loader.load(); 
@@ -59,27 +63,9 @@ public class MainViewController implements Initializable{
 			mainVBox.getChildren().add(mainMenu); // add o mainMenu no mainVBox
 			mainVBox.getChildren().addAll(newVBox.getChildren()); // add os filhos do newVBox
 			// *com isso é possivel manipular a cena principal incluindo nela alem do mainMenu os filhos da janela que estiver sendo aberta no caso a (newVBox)
-		}
-		catch (IOException e) {
-			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-		}
-	}
-	
-	private synchronized void loadView2(String absoluteName) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVBox = loader.load(); 
-			
-			Scene mainScene = Main.getMainScene();
-			VBox mainVBox =  (VBox)((ScrollPane) mainScene.getRoot()).getContent(); 
-			Node mainMenu = mainVBox.getChildren().get(0); 
-			mainVBox.getChildren().clear(); 
-			mainVBox.getChildren().add(mainMenu); 
-			mainVBox.getChildren().addAll(newVBox.getChildren()); 
 		
-			DepartmentListController controller = loader.getController();
-			controller.setDepartmentService(new DepartmentService());
-			controller.updateTableView();
+			T controller = loader.getController();
+			initializingAction.accept(controller);
 		}
 		catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -10,8 +10,8 @@ public class DepartmentService {
 	public List<Department> findAll() {
 		List<Department> list = new ArrayList<>();
 		list.add(new Department(1, "Books"));
-		list.add(new Department(1, "Computers"));
-		list.add(new Department(1, "Eletronics"));
+		list.add(new Department(2, "Computers"));
+		list.add(new Department(3, "Eletronics"));
 		return list;
 	}
 
